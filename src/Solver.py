64cf16bb8eed3c16f3e97dabe5d77b37cfdc7315
GO-AlexTaranukha <ataranukha@gannett.com>
2016-04-19 14:40:31 -0500

    Cleaned up annotation

--- src/Solver.py
+++ src/Solver.py
@@ -1,18 +1,21 @@
 import math
 
+
 class Solver:
     def __init__(self):
         pass
 
     def calculate(self, a, b, c):
+        # type: (object, object, object) -> object
             d = b ** 2 - 4 * a * c
             if d >= 0:
-                discriminant = math.sqrt(d)
-                root1 = (-b + discriminant) / (2 * a)
-                root2 = (-b - discriminant) / (2 * a)
+                disc = math.sqrt(d)
+                root1 = (-b + disc) / (2 * a)
+                root2 = (-b - disc) / (2 * a)
                 print (root1, root2)
             else:
                 print ("invalid quadratic equation")
+
 
 while True:
     a = int(input("a "))
